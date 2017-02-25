118cf86ac30906849e2bd0177a3912301de17c00
Tomy8s <tomy8s@hotmail.com>
2017-02-25 15:54:28 +0000

    removed references to devise

--- db/migrate/20160416164533_create_tefls.rb
+++ db/migrate/20160416164533_create_tefls.rb
@@ -1,7 +1,6 @@
 class CreateTefls < ActiveRecord::Migration
   def change
     create_table :tefls do |t|
-      t.references :user, index: true, foreign_key: true
       t.string :link
       t.text :description
       t.integer :visited
